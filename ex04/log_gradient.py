d709f3eca0bdf1d52c85715e5d070ec72cd36965
clorin <clorin@student.42.fr>
2022-11-16 16:45:50 +0100

    ADD: ex04 et cleaning ex01/log_pred.py

--- ex04/log_gradient.py
+++ ex04/log_gradient.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def sigmoid_(x):
     """
     Compute the sigmoid of a vector.
@@ -59,4 +60,41 @@ def logistic_predict_(x, theta):
 
     except Exception as e:
         print(e)
+        return None
+
+def log_gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatibl
+    Args:
+        x: has to be an numpy.ndarray, a matrix of shape m * n.
+        y: has to be an numpy.ndarray, a vector of shape m * 1.
+        theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+    Returns:
+        The gradient as a numpy.ndarray, a vector of shape (n+1) * 1, containing the result of the formula for all j.
+        None if x, y, or theta are empty numpy.ndarray.
+        None if x, y and theta do not have compatible dimensions.
+    Raises:
+        This function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray) or not isinstance(theta, np.ndarray):
+        print("Error in log_gradient(): x is not a numpy array.")
+        return None
+    if x.size == 0 or y.size == 0:
+        print("Error in log_gradient(): Empty array")
+        return None
+    try:
+        m, n =x.shape
+        if m != y.shape[0] or y.shape[1] != 1 or theta.shape[0] != n + 1:
+            print("Error in log_gradient(): Empty array")
+            return None
+        y_hat = logistic_predict_(x, theta)
+        gradient = np.zeros((n + 1, 1))
+        gradient[0] = (1 / m) * sum(y_hat -y)
+        for j in range(1, n + 1):
+            sum_ = 0
+            for i in range(m):
+                sum_ += (y_hat[i] - y[i]) * x[i][j - 1]
+                gradient[j] = (1 / m) * sum_
+        return gradient
+    except Exception as e:
+        print(f"Error in log_gradient(): {e}")
         return None
