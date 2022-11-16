7fd980feea171b801453ff4d1d4826e66efc5895
clorin <clorin@student.42.fr>
2022-11-16 16:58:33 +0100

    ADD:ex05 Fix msg error type

--- ex05/vec_log_gradient.py
+++ ex05/vec_log_gradient.py
@@ -62,39 +62,34 @@ def logistic_predict_(x, theta):
         print(e)
         return None
 
-def log_gradient(x, y, theta):
-    """Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatibl
+def vec_log_gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.ndarray, without any for-loop. The three arrays must have comp
     Args:
         x: has to be an numpy.ndarray, a matrix of shape m * n.
         y: has to be an numpy.ndarray, a vector of shape m * 1.
-        theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+        theta: has to be an numpy.ndarray, a vector (n +1) * 1.
     Returns:
-        The gradient as a numpy.ndarray, a vector of shape (n+1) * 1, containing the result of the formula for all j.
+        The gradient as a numpy.ndarray, a vector of shape n * 1, containg the result of the formula for all j.
         None if x, y, or theta are empty numpy.ndarray.
-        None if x, y and theta do not have compatible dimensions.
+        None if x, y and theta do not have compatible shapes.
     Raises:
         This function should not raise any Exception.
     """
     if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray) or not isinstance(theta, np.ndarray):
-        print("Error in log_gradient(): x is not a numpy array.")
+        print("Error in vec_log_gradient(): x is not a numpy array.")
         return None
     if x.size == 0 or y.size == 0:
-        print("Error in log_gradient(): Empty array")
+        print("Error in vec_log_gradient(): Empty array")
         return None
     try:
         m, n =x.shape
         if m != y.shape[0] or y.shape[1] != 1 or theta.shape[0] != n + 1:
-            print("Error in log_gradient(): not compatible shape")
+            print("Error in vec_log_gradient(): not compatible shape")
             return None
         y_hat = logistic_predict_(x, theta)
-        gradient = np.zeros((n + 1, 1))
-        gradient[0] = (1 / m) * sum(y_hat -y)
-        for j in range(1, n + 1):
-            sum_ = 0
-            for i in range(m):
-                sum_ += (y_hat[i] - y[i]) * x[i][j - 1]
-                gradient[j] = (1 / m) * sum_
-        return gradient
+        ones = np.ones(len(x)).reshape(-1, 1)
+        x_ = np.concatenate([ones, x], axis=1)
+        return (1 / m) * (x_.T.dot(y_hat - y))
     except Exception as e:
-        print(f"Error in log_gradient(): {e}")
+        print(f"Error in vec_log_gradient(): {e}")
         return None
