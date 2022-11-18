4f67867879be77b173a13abc354bd959d9374c9b
clorin <clorin@student.42.fr>
2022-11-18 17:45:54 +0100

    In progress ... ex07 graph 2D

--- ex07/multi_log.py
+++ ex07/multi_log.py
@@ -19,6 +19,42 @@ red = '\033[91m' # rouge
 reset = '\033[0m' #gris, couleur normale
 planets = ['The flying cities of Venus', 'United Nations of Earth', 'Mars Republic', 'The Asteroids’ Belt colonies']
 
+def graph(fig, axis, data_graph):
+    x = data_graph['x']
+    y = data_graph['y']
+    x_label = data_graph['x_label']
+    y_label = data_graph['y_label']
+    true_planet = data_graph['true_planet']
+    predicted_planet = data_graph['predicted_value']
+    succes = data_graph['succes']
+    error = data_graph['error']
+
+    planet_colors = ['r', 'b', 'c', 'y']
+    planet_names = [planets[int(i)] for i in true_planet]
+    colors_true = [planet_colors[int(i[0])] for i in true_planet]
+    colors_predicted = [planet_colors[i] for i in predicted_planet]
+    pdf = pd.DataFrame({'x':x, "y":y, 'colors':colors_true, 'planet':planet_names})
+    for planet, dff in pdf.groupby('planet'):
+        axis.scatter(dff['x'], dff['y'], c=dff['colors'], marker='o', label=planet)
+    axis.scatter(x, y, c=colors_predicted, marker='x', label=None)
+    axis.set_xlabel(x_label)
+    axis.set_ylabel(y_label)
+    axis.legend()
+
+def best_result(array):
+    """ return the num colomun so the num of planet"""
+    if len(array) != 4:
+        print("Error in best_result(): bad array")
+        return None
+    column_val = array[0]
+    column = 0
+    for i in range(1, 4):
+        if array[i] > column_val:
+            column_val = array[i]
+            column = i
+    return column
+
+
 def main():
     bio, citi = loading()
     #split
@@ -33,12 +69,51 @@ def main():
         #logistic regression
         print(f"Training to Citizens of '{green}{planets[zipcode]}{reset}' ...")
         thetas = np.array(np.ones(4)).reshape(-1, 1)
-        mylr = MyLR(thetas, alpha=0.1, max_iter=50000)
+        mylr = MyLR(thetas, alpha=0.1, max_iter=1000)
         mylr.fit_(x_train_, citizens_filtred)
         list_reg.append(mylr.predict_(x_test_))
+
+    concat = np.concatenate((list_reg[0], list_reg[1], list_reg[2], list_reg[3]), axis=1)
+
     
-    for i in range(len(y_test)):
-        print(f"{list_reg[0][i]} - {list_reg[1][i]} - {list_reg[2][i]} - {list_reg[3][i]}")
+    print("analyze ...")
+    predicted_value = []
+    score = 0
+    for i, (c, y) in enumerate(zip(concat, y_test)):
+        print(f"{i}", end='')
+        best = best_result(c)
+        predicted_value.append(best)
+        print(f" -> {planets[best]}", end='')
+        if best == y[0]:
+            print(f" = {green}{planets[int(y[0])]}{reset}")
+            score += 1
+        else:
+            print(f" != {red}{planets[int(y[0])]}{reset}")
+    succes = score / len(y_test) * 100
+    error = 100 - succes
+    print(f"Score : succes = {yellow}{succes:0.2f}%{reset}\terror = {yellow}{error:0.2f}%{reset}")
+
+
+    #graph 2D
+    fig, axis = plt.subplots(3, 1, figsize=(15, 8))
+    fig.suptitle("Succes = {:.2f}% - Error = {:.2f}%".format(succes, error), fontsize=12)
+
+    graph(fig, axis[0], {'x':x_test[:, 0], 'y':x_test[:, 1],
+                'x_label': 'Weight', 'y_label':'Height',
+                'true_planet':y_test, 'predicted_value':predicted_value,
+                'succes':succes,'error':error})
+
+    graph(fig, axis[1], {'x':x_test[:, 0], 'y':x_test[:, 2],
+                'x_label': 'Weight', 'y_label':'Bone density',
+                'true_planet':y_test, 'predicted_value':predicted_value,
+                'succes':succes,'error':error})
+    graph(fig, axis[2], {'x':x_test[:, 2], 'y':x_test[:, 1],
+                'x_label': 'Bone density', 'y_label':'Height',
+                'true_planet':y_test, 'predicted_value':predicted_value,
+                'succes':succes,'error':error})
+    
+    plt.show()
+
 if __name__ == "__main__":
     main()
     print("good bye !")
